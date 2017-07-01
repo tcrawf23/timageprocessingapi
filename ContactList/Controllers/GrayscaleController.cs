3d43aee38d7c5fff041810c65f59b9a6192d0b2b
Tim <tcrawf23@gmail.com>
2017-06-30 21:42:52 -0700

    Add grayscale controller and rename contacts to kmeans

---
+++ ContactList/Controllers/GrayscaleController.cs
@@ -0,0 +1,67 @@
+﻿using System.Threading.Tasks;
+using System.Web.Http;
+using System.IO;
+using System.Net.Http;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System;
+
+namespace ContactList.Controllers
+{
+    public class GrayscaleController : ApiController
+    {
+        public async Task<HttpResponseMessage> Post()
+        {
+            byte[] fileData = await Request.Content.ReadAsByteArrayAsync();
+
+            if (fileData == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            Bitmap bitmap = new Bitmap(new MemoryStream(fileData));
+
+
+            //create a blank bitmap the same size as original
+            Bitmap newBitmap = new Bitmap(bitmap.Width, bitmap.Height);
+
+            //get a graphics object from the new image
+            Graphics g = Graphics.FromImage(newBitmap);
+
+            //create the grayscale ColorMatrix
+            ColorMatrix colorMatrix = new ColorMatrix(
+            new float[][]
+            {
+                new float[] {.3f, .3f, .3f, 0, 0},
+                new float[] {.59f, .59f, .59f, 0, 0},
+                new float[] {.11f, .11f, .11f, 0, 0},
+                new float[] {0, 0, 0, 1, 0},
+                new float[] {0, 0, 0, 0, 1}
+            });
+            
+            //create some image attributes
+            ImageAttributes attributes = new ImageAttributes();
+            
+            //set the color matrix attribute
+            attributes.SetColorMatrix(colorMatrix);
+            
+            //draw the original image on the new image
+            //using the grayscale color matrix
+            g.DrawImage(bitmap, new Rectangle(0, 0, bitmap.Width, bitmap.Height),
+            0, 0, bitmap.Width, bitmap.Height, GraphicsUnit.Pixel, attributes);
+            
+            //dispose the Graphics object
+            g.Dispose();
+
+            MemoryStream ms = new MemoryStream();
+            newBitmap.Save(ms, ImageFormat.Jpeg);
+            fileData = ms.ToArray();
+
+            HttpResponseMessage Response = new HttpResponseMessage(HttpStatusCode.OK);
+            Response.Content = new ByteArrayContent(fileData);
+            Response.Content.Headers.ContentType = new MediaTypeHeaderValue("image/jpeg");
+
+            return Response;
+        }
+    }
+}
--- ContactList/Controllers/KMeansController.cs
+++ ContactList/Controllers/KMeansController.cs
@@ -1,8 +1,5 @@
-﻿using ContactList.Models;
-using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using System.Web.Http;
-using System.Linq;
 using System.IO;
 using System.Net.Http;
 using System.Net;
@@ -13,31 +10,8 @@ using System;
 
 namespace ContactList.Controllers
 {
-    public class ContactsController : ApiController
+    public class KMeansController : ApiController
     {
-        private Contact[] GetContacts()
-        {
-            var contacts = new Contact[]{
-                        new Contact { Id = 1, EmailAddress = "barney@contoso.com", Name = "Barney Poland"},
-                        new Contact { Id = 2, EmailAddress = "lacy@contoso.com", Name = "Lacy Barrera"},
-                        new Contact { Id = 3, EmailAddress = "lora@microsoft.com", Name = "Lora Riggs"},
-                        new Contact { Id = 4, EmailAddress = "jelly@flavor.com", Name = "Peanut Butter"}
-                        };
-
-            return contacts;
-        }
-
-        public IEnumerable<Contact> GetAllContacts()
-        {
-            return GetContacts();
-        }
-
-        public Contact Get([FromUri] int id)
-        {
-            var contacts = GetContacts();
-            return contacts.FirstOrDefault(x => x.Id == id);
-        }
-
         public async Task<HttpResponseMessage> Post([FromUri] int numClusters)
         {
             byte[] fileData = await Request.Content.ReadAsByteArrayAsync();
